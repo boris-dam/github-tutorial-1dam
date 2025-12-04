b1a5d5871396b3eb874c891b1c2e9cb495e61d61
seovermig <seovermig@PC20930.iespiringalla.ad>
2025-12-04 11:10:08 +0100

    Miguel Seoane

---
+++ src/main/java/es/dam/collaborations/MiguelSeoane.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.dam.collaborations;
+
+/**
+ *
+ * @author seovermig
+ */
+public class MiguelSeoane {
+     public static void collaboration() {
+        System.out.println("Hola Mundo/Boris");
+    }
+}
