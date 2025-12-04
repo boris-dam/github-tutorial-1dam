895732827c9ae5a6cd2962e016669ce915eaa189
gonalvile <ileniaglezalvarez@gmail.com>
2025-12-04 11:14:19 +0100

    Prueba Ilenia

---
+++ src/main/java/es/dam/collaborations/IleniaGonzalez.java
@@ -0,0 +1,9 @@
+package es.dam.collaborations;
+
+public class IleniaGonzalez {
+
+    public static void Ilenia(String[] args) {
+        System.out.println("Hola, esto es una prueba");
+    }
+
+}
